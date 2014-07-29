9ae7053b9b08505bf01beb6e85f15481965fb557
Matt Collins <matt@scoot.co.uk>
2014-07-29 09:13:34 +0100

    thanks page

---
+++ views/thanks.php
@@ -0,0 +1,29 @@
+<!--Page Title-->
+<section id="page-title">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 title-white">
+        <h2>Thanks!</h2>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<!--Content Section-->
+<section class="content mt40">
+  <div class="container">
+    
+    <div class="row">
+      <div class="col-sm-12 col-md-6">
+        
+        <h1 class="red">Thanks for getting in touch!</h1>
+
+        <p>I will read and answer your request as soon as possible, in the mean time why don't you check out my <a href="/blog">Blog</a> for some of my top fitness tips, or watch some of the <a href="/">videos</a> on my homepage.</p>
+        <p>- Troy</p>
+        
+      </div>
+      
+    </div><!--End Row-->   
+  </div>
+</section> 
